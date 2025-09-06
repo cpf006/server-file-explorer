c194c4a160ed0746d6bd44c37a4fa7eb7b673d2c
Connor Ford <cpf006@gmail.com>
2025-09-06 01:49:47 -0400

    Simplify PathResolver registration

--- FileExplorerOptions.cs
+++ FileExplorerOptions.cs
@@ -4,7 +4,9 @@ namespace TestProject;
 
 public class FileExplorerOptions
 {
+    public const string DefaultRootDirectoryName = "DefaultDirectory";
+
     // Root directory for all file work. The spec asks for a configurable home folder, so we default to a sandboxed "DefaultDirectory".
-    public string RootPath { get; set; } = Path.Combine(AppContext.BaseDirectory, "DefaultDirectory");
+    public string RootPath { get; set; } = Path.Combine(AppContext.BaseDirectory, DefaultRootDirectoryName);
 }
 
